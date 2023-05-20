7904344f789d04705a01ce95fdbc30ea78cd8a12
Fevin Patel <fevin@fevins-air.home>
2023-05-20 16:48:30 -0400

    Added funtionalities of passport and authentication to adapt Cognito JWKS

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,13 +5,17 @@ const express = require('express');
 // version and author from package.json
 const { version, author } = require('../../package.json');
 
+// Our authentication middleware
+const { authenticate } = require('../authentication');
+
 // Create a router that we can use to mount our API
 const router = express.Router();
 
 /**
  * Expose all of our API routes on /v1/* to include an API version.
  */
-router.use(`/v1`, require('./api'));
+router.use(`/v1`, authenticate(), require('./api'));
+//router.use(`/v1`, require('./api'));
 
 /**
  * Define a simple health check route. If the server is running
